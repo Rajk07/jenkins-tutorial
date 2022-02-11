f2495d9a8eb9ba33093188544356fe76cbffc7a0
Alagar Rajkumar <967rajkumar@gmail.com>
2022-02-11 15:03:38 +0530

    added a successful test case

---
+++ src/test/java/com/in28minutes/rest/dao/UserDAOServiceTest.java
@@ -0,0 +1,28 @@
+package com.in28minutes.rest.dao;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.in28minutes.rest.RestWebServiceDemoApplication;
+import com.in28minutes.rest.entity.User;
+
+@SpringBootTest(classes= {RestWebServiceDemoApplication.class})
+class UserDAOServiceTest {
+
+	@Autowired
+	UserDAOService userDAOService;
+	
+	@Test
+	void testGetUsers() {
+		List<User> allUsers = userDAOService.getUsers();
+		assertFalse(allUsers.isEmpty());
+		assertEquals(3, allUsers.size());
+	}
+
+}
